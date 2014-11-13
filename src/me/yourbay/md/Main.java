8dd53b1bfc23d1bcf7ff8ab781f8df822243261a
hyongbai <ram@hyongbai.local>
2014-11-14 00:04:01 +0800

    fix import

--- src/me/yourbay/md/Main.java
+++ src/me/yourbay/md/Main.java
@@ -1,7 +1,10 @@
-package com.example.md;
+package me.yourbay.md;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 
 /**
  * @project md
